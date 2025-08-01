bcbc8efae0691850d807035e9ac754dc5e10e34c
Akash Singh <mr.singhaksh@gmail.com>
2025-08-01 13:29:13 +0530

    Fix: Restore accidentally deleted index.html

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import LoanTreeVisualizer from './App.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <LoanTreeVisualizer/>
+    <App/>
   </StrictMode>,)
